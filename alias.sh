e20b2b38acec2c085e595c0b0ac99ae00f837051
whoslcy <94728556+whoslcy@users.noreply.github.com>
2023-03-07 22:01:06 +0800

    Update alias.sh
    Change a git alias "lol" to a bash alias "gl".

--- alias.sh
+++ alias.sh
@@ -1,2 +1,3 @@
-git config --global alias.lol "log --graph --oneline --decorate --color --all"
 alias gs="git status"
+#git config --global alias.lol "log --graph --oneline --decorate --color --all"
+alias gl="git log --graph --oneline --decorate --color --all"
