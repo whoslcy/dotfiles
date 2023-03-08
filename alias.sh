e5d96d53cf931f732cc777b18f63751a2374a4cf
whoslcy <94728556+whoslcy@users.noreply.github.com>
2023-03-08 13:19:57 +0800

    Update alias.sh
    Add an alias of git add.

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
 alias gs="git status"
 #git config --global alias.lol "log --graph --oneline --decorate --color --all"
 alias gl="git log --graph --oneline --decorate --color --all"
+alias ga="git add"
