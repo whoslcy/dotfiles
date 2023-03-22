2785d95c13ec4be19cfbac01fae530e20047536b
whoslcy <94728556+whoslcy@users.noreply.github.com>
2023-03-22 19:34:30 +0800

    Update and rename alias.sh to initialize.sh
    Add some commands about apt install.

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,20 @@
+#!/usr/bin/env bash
+
 alias gs="git status"
 #git config --global alias.lol "log --graph --oneline --decorate --color --all"
 alias gl="git log --graph --oneline --decorate --color --all -10"
 alias ga="git add"
 alias gc="git commit"
 alias gb="git branch"
+
+sudo apt update
+sudo apt install \
+git \
+vim \
+wget \
+curl \
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+if [[ $(command -v nvm) -eq 'nvm' ]]; then
+nvm install --lts
